130937694e9c5854ef750728893c6c6f07b4a05e
Jimmy31101 <jaimemuozmarcelo@gmail.com>
2025-11-30 22:22:21 +0100

    Correjido EJ07 UD03_FORO

--- UD03_FORO/EJ07.py
+++ UD03_FORO/EJ07.py
@@ -5,10 +5,18 @@ n = 7
 #Print de las filas
 for i in range(n):
 
+    #Print de columnas
     for j in range(n):
-        if j == 0 or j == n - 1 or i == 0 or i == n - 1 or j == 2 or j == 3 or j == 4 :
+
+        #Print de huecos para las diagonales
+        if (j == 5 and i == 4) or (j == 5 and i == 2):
+            print(" ", end="")
+
+        #Print de figura con diagonales
+        elif j == 0 or j == n - 1 or i == 0 or i == n - 1 or i == j - 1 or i + j == n:
             print("*", end="")
 
+        #Print de huecos
         else:
             print(" ", end="")
     print("\n")
