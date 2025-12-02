a950d9d940df8aa8036f66c5158fd9c24cad4bc5
Jimmy31101 <jaimemuozmarcelo@gmail.com>
2025-12-02 20:15:38 +0100

    Añadido ejercicio 10 UD03_FORO/FIGURAS

--- UD03_FORO/EJ_FIGURES/EJ10.py
+++ UD03_FORO/EJ_FIGURES/EJ10.py
@@ -10,19 +10,6 @@
 
 n = 6
 
-#Print de la pirámide invertida
-for i in range(n):
-
-    for j in range(i, n):
-        print("*", end="")
-
-    for j in range(n, 0, - 1):
-        if j > i:
-            print("", end="")
-        else:
-            print("*", end="")
-    print()
-print("\n")
 #Print con matriz cuadrada
 for i in range(n):
 
@@ -30,8 +17,11 @@ for i in range(n):
         if i== 0 or i == n - 1:
             print("*", end="")
 
-        elif j == 0 or j == n - 1 or (i == 1 and j % 2 == 0) or (i == 3 and j % 2 == 0):
+        elif j == 0 or (i % 2 != 0 and j % 2 == 0) or (i % 2 != 0 and j % 2 == 0):
             print("*", end="")
+
+        elif j == n - 1:
+            print(" *", end="")
 
         else:
             print(" ", end="")
